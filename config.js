a5bdfb4be391ed272fe9d374e4b93b712f7d4e73
AKU SAYANG PACAR MU <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-31 21:05:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -47,15 +47,14 @@ global.author = '091102'
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© BOT TZY' //change the watermark 
-global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/74ee3213700db6534e59a.png'
-
+global.watermark = '© bot tjy' //change the watermark 
+global.image = 'https://telegra.ph/file/1a3eac3701775793618b3.jpg" //change the image
+global.thumbfoto = 'https://telegra.ph/file/1a3eac3701775793618b3.jpg"
 // image
-global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
-global.kandang = 'https://telegra.ph/file/67a6ee607d03a4e52757d.jpg'
-global.kolam = 'https://telegra.ph/file/5aa5dfa3394477e11fb18.jpg'
-global.thanks = 'https://telegra.ph/file/01917f7782b70de8f418d.jpg'
+global.bank = 'https://telegra.ph/file/79197c44a99b24dfdfe71.jpg'
+global.kandang = 'https://telegra.ph/file/79197c44a99b24dfdfe71.jpg'
+global.kolam = 'https://telegra.ph/file/79197c44a99b24dfdfe71.jpg'
+global.thanks = 'https://telegra.ph/file/79197c44a99b24dfdfe71.jpg'
 
 global.multiplier = 9999999 // Semakin tinggi, semakin sulit naik level
 
